294beecf91f37de7c879cfa1281b9dccc9ab8102
Levente Gárdonyi <levente.gardonyi@stud.uni-corvinus.hu>
2024-05-16 17:59:32 +0200

    Add project files.

---
+++ tapajanlo/Controllers/SziaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tapajanlo.dnn.tapajanlo.Controllers
+{
+    public class SziaController : Controller
+    {
+        // GET: Szia
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
